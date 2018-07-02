0ec464760196290ab5e1a7270ae971b1238d5129
twumm <martint.mensah@gmail.com>
2018-07-02 22:29:44 +0000

    feat: Add function to convert currency. Do not reload page by invoking preventDefault() method.

--- app/main.js
+++ app/main.js
@@ -15,11 +15,11 @@ fetch(converterURL).then(response => {
   }
   response.json().then(currencies => {
     for (let currency in currencies.results) {
-      currencyName = currencies.results[currency].currencyName;
+      // currencyName = currencies.results[currency].currencyName;
       currencyCode = currencies.results[currency].id;
       // console.log(currencyName, currencyCode);
       option = document.createElement('option');
-      option.innerText = `${currencyCode} - ${currencyName}`;
+      option.innerText = `${currencyCode}`;
       option.id = currencyCode;
       currencyFrom.appendChild(option.cloneNode(true));
       currencyTo.appendChild(option);
@@ -27,64 +27,28 @@ fetch(converterURL).then(response => {
   })
 });
 
-
-// let result = document.getElementById('convertedAmount');
-
-/*let convertCurrency = () => {
-    let fromCurrency = document.getElementById('currencyFrom').value;
-    let toCurrency = document.getElementById('currencyTo').value;
-    let amount = document.getElementById('amount').value;
-    let result = document.getElementById('convertedAmount');
-   
-
-    let query = fromCurrency + '_' + toCurrency;
-    let url = 'https://www.currencyconverterapi.com/api/v5/convert?q=' + query + '&compact=y';
-    fetch(url)
-        .then((response) => {
-            response.json()
-        }).then((data) => {
-            console.log(data)
-            let val = data[query].val;
-            console.log(val);
-            console.log(amount);
-            console.log('something here');
-
-            if (val != undefined) {
-                let total = parseFloat(val) * parseFloat(amount);
-                result.innerHTML = total;
-                console.log(result);
-            } 
-
-        })
-}*/
-
-/*convertButton.addEventListener('click', () => {
-  let fromCurrency = document.getElementById('currencyFrom').value;
-  let toCurrency = document.getElementById('currencyTo').value;
-  // console.log(fromCurrency, toCurrency);
-  result.innerText = 'Awesome'
-  document.addEventListener('DOMContentLoaded', function() {
-    console.log(fromCurrency, toCurrency);
-  })
-})*/
-
-convertButton.addEventListener('click', () => {
+convertButton.addEventListener('click', e => {
 // $('#convertButton').on('click', () => {
+  e.preventDefault();
   const fromCurrency = document.getElementById('currencyFrom').value;
   const toCurrency = document.getElementById('currencyTo').value;
   const amount = document.getElementById('amount').value;
   const result = document.getElementById('convertedAmount');
   const currencyIDsToConvert = fromCurrency + '_' + toCurrency;
-  const queryUrl = 'https://www.currencyconverterapi.com/api/v5/convert?q=' + currencyIDsToConvert + '&compact=y';
+  const queryUrl = 'https://free.currencyconverterapi.com/api/v5/convert?q=' + currencyIDsToConvert + '&compact=y';
+  // let total;
+  console.log(fromCurrency, toCurrency, amount);
 
   fetch(queryUrl).then(response => {
     response.json().then(rates => {
       console.log(rates);
       for(let rate in rates) {
-        console.log(rate);
+        console.log(rates[rate].val);
         let value = rates[rate].val;
       let total = Number(amount) * value;
-      result.val(Math.round(total * 100) /100);
+      total = Math.round(total * 100) /100;
+      console.log(total);
+      result.value = `${total}`;
       }
     })
   })
