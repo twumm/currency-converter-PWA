8274b315d30a930b2932b95fc49c35c2f1caacef
twumm <martint.mensah@gmail.com>
2018-07-03 09:23:34 +0000

    feat: Intercept network requests and serve static files when offline.

--- service-worker.js
+++ service-worker.js
@@ -7,12 +7,38 @@ const filesToCache = [
   '/images/icon.png'
 ];
 
-self.addEventListener('install', e => {
+// Install service worker and assets/files to cache.
+self.addEventListener('install', event => {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
+  event.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log(`[ServiceWorker] Caching app shell`);
+      console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
+  );
+});
+
+// Activate service worker.
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheName && key !==dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
   )
-})
+});
+
+// Intercept requests and return cached version of assets.
+self.addEventListener('fetch', event => {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
